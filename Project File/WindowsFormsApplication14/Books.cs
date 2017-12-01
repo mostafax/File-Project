333fd1d71df4fea25b346f294a776776bbf2c104
mostafa mohamed <mostafamousa1080p@gmail.com>
2017-12-01 16:52:10 +0200

    Hushis Update

--- Project File/WindowsFormsApplication14/Books.cs
+++ Project File/WindowsFormsApplication14/Books.cs
@@ -19,7 +19,7 @@ static public class BooksFunctions
         StreamReader sr = new StreamReader(fs);
         string[] records, fields;
         records = sr.ReadToEnd().Split('#');
-        if(records.Length == 0)
+        if (records.Length == 0)
         {
             return null;
         }
@@ -35,6 +35,44 @@ static public class BooksFunctions
         }
         sr.Close();
         return books;
+    }
+    static public Book[] Search(string s)
+    {
+        FileStream Fs = new FileStream("Book.txt", FileMode.Open);
+        StreamReader sr = new StreamReader(Fs);
+        string[] records, fields;
+        int[] saver = new int[10];
+        int x = 0;
+        bool found = false;
+        records = sr.ReadToEnd().Split('#');
+        sr.Close();
+        for (int i = 0; i < records.Length - 1; i++)
+        {
+            fields = records[i].Split('@');
+            if (s == fields[2])
+            {
+                saver[x] = i;
+                found = true;
+                x++;
+            }
+        }
+        if (found == false)
+        {
+            return null;
+        }
+        Book[] books = new Book[x];
+
+        for (int i = 0; i < x; i++)
+        {
+           
+            fields = records[saver[i]].Split('@');
+            books[i].serialNo = fields[0];
+            books[i].bookName = fields[1];
+            books[i].publishYear = fields[2];
+            books[i].authorNo = fields[3];
+        }
+        return books;
+
     }
 
 
